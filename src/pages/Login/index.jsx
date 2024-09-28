86c1776ae9615b9bead507db1beea58093521235
noor-ridha <noorridha028@gmail.com>
2024-09-28 12:26:21 +0300

    add styling to login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,27 +4,31 @@ import Calendar from '../../../public/CalendarPic.svg'
 
 const Login = () => {
   return (
-    <div className='flex justify-between bg-FFFFFF'>
-      <div className='login side mx-10 mt-40'>
-       <form>
-        <div className='text-center text-lg font-bold text-black'>تسجيل الدخول </div>
+    <div className='flex justify-center bg-FFFFFF'>
+      <div className='login side mx-10 mt-40  w-[50%] h-full'>
+       <form className='border border-slate-200 rounded-lg w-[50%] h-[100%]'> 
+        <div className='text-center text-lg font-bold text-black mx-2 mt-3'>تسجيل الدخول </div>
         <div className='telephone input flex flex-col mt-8'>
-        <label className='text-right '> رقم الهاتف</label>
-          <input type='tel' className='bg-slate-100 text-gray-700 rounded-xl mt-3' /> 
+        <label className='text-right mx-2'> رقم الهاتف</label>
+          <input type='tel' className='bg-slate-100 text-gray-700 rounded-lg mt-3 mx-2 w-[90%]' /> 
           
         </div>
         <div className='password input flex flex-col mt-5'>
-        <label className='text-right '> كلمة السر</label>
-        <input type='password' className='bg-slate-100 text-gray-700 rounded-xl mt-3'/>
+        <label className='text-right mx-2'> كلمة السر</label>
+        <input type='password' className='bg-slate-100 text-gray-700 rounded-lg mt-3 mx-2 w-[90%]'/>
          
           
 
         </div>
-        <div className='text-right mt-5 text-orange-500'>هل نسيت كلمة المرور</div>
-        <button>تسجيل الدخول</button>
-        <div>
-        <p>هل لديك حساب جديد؟ </p>
+        <div className='text-right mt-5 mx-2 text-orange-500'>هل نسيت كلمة المرور؟</div>
+        <div className='flex justify-center'>
+        <button className='w-[90%] border bg-orange-600 rounded mx-2 mt-4 h-10'>تسجيل الدخول</button>
+        </div>
+        
+        <div className='mt-4 mx-2 mb-4 flex justify-end space-x-2 '> 
         <p>تسجيل جديد</p>
+        <p >هل لديك حساب جديد؟ </p>
+        
         </div>
        </form>
 
