bde5b20c022e50eb3e89f14b449e350cd5e50ff3
noor-ridha <noorridha028@gmail.com>
2024-10-01 00:12:32 +0300

    connect the signUp to the backend and successfully registering users

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../../Styles/globals.css";
 import Image from "next/image";
 import SignUpPic from "../../../public/SignUpPic.svg";
@@ -6,14 +6,15 @@ import SignUpPic from "../../../public/SignUpPic.svg";
 const SignUp = () => {
   // form field state
   const [formData, setFormData] = useState({
-    countryCode: "+966",
+    // countryCode: "+966",
     phone: "",
+    role: "customer",
     email: "",
     password: "",
-    confirmPassword: "",
+    confirm_password: "",
   });
 
-  // Handle input change
+  // Handling input changes
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -22,22 +23,24 @@ const SignUp = () => {
     });
   };
 
-  // Handle form submission
+  // Handling the form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Optional: Validate form fields (e.g., check if passwords match)
+    //  Validate form fields
     if (formData.password !== formData.confirmPassword) {
       alert("Passwords do not match");
       return;
     }
 
     // Prepare data to send to the API
-    const dataToSend = {
-      country_code: formData.countryCode,
-      phone: formData.phone,
+    const dataToSendToApi = {
+      // country_code: formData.countryCode,
+      phone_number: formData.phone,
       email: formData.email,
       password: formData.password,
+      confirm_password: formData.confirmPassword,
+      role: formData.role,
     };
 
     try {
@@ -49,7 +52,7 @@ const SignUp = () => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(dataToSend),
+          body: JSON.stringify(dataToSendToApi),
         }
       );
 
@@ -70,62 +73,105 @@ const SignUp = () => {
     }
   };
 
+  // Ensure the component is rendered only on the client to prevent hydration issues
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true); // Set to true when component is mounted on the client
+  }, []);
+
+  if (!isMounted) {
+    // Return null during SSR to avoid hydration issues
+    return null;
+  }
+
   return (
     <div className="flex justify-center bg-FFFFFF ">
-      <div className="login side w-[50%] h-full ">
-        <form className="border border-slate-200 rounded-lg w-[60%] h-[100%] mx-10 mt-40">
+      <div className="login side w-[65%] h-full ">
+        <form
+          onSubmit={handleSubmit}
+          className="border border-slate-200 rounded-lg w-[517px] h-auto mx-10 mt-40"
+        >
           <div className=" font-tajawal text-center text-lg font-bold text-black mx-2 mt-3">
             تسجيل الدخول{" "}
           </div>
-          <div className="telephone input flex flex-col mt-8">
-            <label className=" font-tajawal text-right mx-2"> رقم الهاتف</label>
-            <div className="flex justify-center space-x-2">
-              <select
-                name="countryCode"
-                class="border rounded-lg h-10 mt-3 w-20 bg-slate-100 text-gray-700"
-              >
-                <option value="+1">+966 (KSA)</option>
-                <option value="+44">+44 (UK)</option>
-                <option value="+91">+91 (India)</option>
-              </select>
+          <div className="flex flex-col items-end">
+            <div className="telephone input flex flex-col mt-8">
+              <label className=" font-tajawal text-right mx-2">
+                {" "}
+                رقم الهاتف
+              </label>
+              <div className=" space-x-1 mx-6">
+                <select
+                  name="countryCode"
+                  // value={formData.countryCode} // Ensure value is set properly
+                  // onChange={handleInputChange}
+                  className=" bg-[#F5F5F5] border rounded-lg h-10 mt-3 w-[88px] h-[48px] text-gray-700"
+                >
+                  <option value="+966">+966 (KSA)</option>
+                </select>
+                <input
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleInputChange}
+                  placeholder="ادخل رقم هاتفك"
+                  required
+                  type="tel"
+                  className=" border bg-[#F5F5F5] text-gray-700 rounded-lg mt-3 w-[375px] h-[48px]"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col mt-8 ">
+              <label className="font-tajawal text-right mx-2">
+                {" "}
+                البريد الالكتروني
+              </label>
+
               <input
-                type="tel"
-                className=" border bg-slate-100 text-gray-700 rounded-lg mt-3 w-60 h-10"
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                placeholder="أدخل البريد الالكتروني الخاص بك"
+                className="border bg-[#F5F5F5] text-gray-700 rounded-lg mt-3 mx-6 w-[469px] h-[48px] "
               />
             </div>
-          </div>
-          <div className="flex flex-col mt-8 ">
-            <label className="font-tajawal text-right mx-2">
-              {" "}
-              البريد الالكتروني
-            </label>
 
-            <input
-              type="email"
-              className="border bg-slate-100 text-gray-700 rounded-lg mt-3 w-80 h-10 mx-16"
-            />
-          </div>
+            <div className="password input flex flex-col mt-5">
+              <label className="font-tajawal text-right mx-2">
+                {" "}
+                كلمة المرور
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                placeholder="أدخل كلمة المرور"
+                required
+                className="border bg-[#F5F5F5] text-gray-700 rounded-lg mt-3 mx-6 w-[469px] h-[48px]"
+              />
+            </div>
+            <div className="password input flex flex-col mt-5">
+              <label className=" font-tajawal text-right mx-2">
+                {" "}
+                أعد كلمة المرور
+              </label>
+              <input
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
+                required
+                placeholder="أعد كتابة كلمة المرور"
+                className=" border bg-[#F5F5F5] text-gray-700 rounded-lg mt-3 mx-6 w-[469px] h-[48px]"
+              />
+            </div>
 
-          <div className="password input flex flex-col mt-5">
-            <label className="font-tajawal text-right mx-2"> كلمة المرور</label>
-            <input
-              type="password"
-              className="bg-slate-100 text-gray-700 rounded-lg mt-3 w-80 h-10 mx-16"
-            />
-          </div>
-          <div className="password input flex flex-col mt-5">
-            <label className=" font-tajawal text-right mx-2">
-              {" "}
-              أعد كلمة المرور
-            </label>
-            <input
-              type="password"
-              className="bg-slate-100 text-gray-700 rounded-lg mt-3 w-80 h-10 mx-16"
-            />
-          </div>
-
-          <div className="flex justify-center">
-            <button className=" border bg-orange-600 rounded-lg  mt-8 h-12 w-80 h-10 mx-16">
+            <button
+              type="submit"
+              className=" border bg-orange-600 rounded-lg  mt-8 mx-6 w-[469px] h-[65px]"
+            >
               تسجيل الدخول
             </button>
           </div>
@@ -147,11 +193,11 @@ const SignUp = () => {
             اكتشف تجربة مميزة للحجوزات
           </p>
         </div>
-        <div className="mt-20">
+        <div className="mt-20 mx-10">
           <Image
             src={SignUpPic}
             alt="Calendar"
-            width={300}
+            width={450}
             height={200}
           ></Image>
         </div>
